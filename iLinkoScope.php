9c802d8761fd89e77d9df48a8e90800122c7837a
Nabeel Sulieman <nabsul@outlook.com>
2015-10-16 09:37:05 -0700

    Add PHPDoc to iLinkoScope

--- iLinkoScope.php
+++ iLinkoScope.php
@@ -10,21 +10,89 @@ namespace ShortCirquit\LinkoScopeApi;
 
 use ShortCirquit\LinkoScopeApi\Models\Link;
 use ShortCirquit\LinkoScopeApi\Models\Comment;
+use ShortCirquit\LinkoScopeApi\Models\UserProfile;
 
 interface iLinkoScope
 {
     function getConfig();
+
+    /**
+     * @return Link[]
+     */
     function getLinks();
+
+    /**
+     * @param $id
+     * @return Link
+     */
     function getLink($id);
+
+    /**
+     * @param Link $link
+     * @return Link Updated link
+     */
     function addLink(Link $link);
+
+    /**
+     * @param Link $link
+     * @return Link
+     */
     function updateLink(Link $link);
+
+    /**
+     * @param $id
+     * @return Link
+     */
     function deleteLink($id);
+
+    /**
+     * @param      $id
+     * @param int|null $userId
+     * @return int number of likes
+     */
     function likeLink($id, $userId = null);
+
+    /**
+     * @param      $id
+     * @param int|null $userId
+     * @return int number of likes
+     */
     function unlikeLink($id, $userId = null);
+
+    /**
+     * @param      $id
+     * @param int|null $userId
+     * @return int number of likes
+     */
     function likeComment($id, $userId = null);
+
+    /**
+     * @param      $id
+     * @param int|null $userId
+     * @return int number of likes
+     */
     function unlikeComment($id, $userId = null);
+
+    /**
+     * @return UserProfile
+     */
     function getAccount();
+
+    /**
+     * @param $postId
+     * @return Comment[]
+     */
     function getComments($postId);
+
+    /**
+     * @param Comment $comment
+     * @return Comment
+     */
     function addComment(Comment $comment);
+
+    /**
+     * @param $id
+     * @return Comment
+     */
     function deleteComment($id);
 }
