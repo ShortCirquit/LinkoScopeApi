e7e20e819b1fac4de29cbaf8ac43a68a812253e6
Nabeel Sulieman <nabsul@outlook.com>
2015-10-16 10:00:26 -0700

    Add UserProfile model
    Missed this. Should have been checked in a couple of commits earlier.

---
+++ Models/UserProfile.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nabeel
+ * Date: 2015-10-15
+ * Time: 6:40 PM
+ */
+
+namespace ShortCirquit\LinkoScopeApi\Models;
+
+
+class UserProfile extends BaseModel
+{
+    public $id;
+    public $username;
+}
