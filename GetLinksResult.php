6803a10e680b89803bc9a94c3d9cd2949d900fc5
Nabeel Sulieman <nabsul@outlook.com>
2015-10-21 21:46:38 -0700

    Create GetLinksResults object
    This stores but the array of actual links returned, as well as the additional information of total
    available and offset.

---
+++ GetLinksResult.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nabeel
+ * Date: 2015-10-21
+ * Time: 12:53 PM
+ */
+
+namespace ShortCirquit\LinkoScopeApi;
+
+
+class GetLinksResult
+{
+    public $links;
+    public $totalResults;
+    public $offset;
+}
